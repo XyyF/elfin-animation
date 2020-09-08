4bb347c0687cf2552048f8452262b700107d5c28
xiaoyufei <915724526@qq.com>
2020-09-08 19:09:59 +0800

    feat: css margin.js

--- css/margin.js
+++ css/margin.js
@@ -2,18 +2,18 @@
  * Created by rengar on 2020/9/8.
  */
 
-void function transform() {
+void function margin() {
     const btnWrapper = document.querySelector('.body__btn-wrapper')
     const detailWrapper = document.querySelector('.body__detail')
 
     function initButton() {
         const btn = document.createElement('button')
-        btn.innerText = 'transform动画'
+        btn.innerText = 'margin动画'
         const animation = document.querySelector('.animation')
         const bodyDetail = document.querySelector('.body__detail')
         btn.addEventListener('click', () => {
-            animation.className = 'animation animation__transform'
-            bodyDetail.className = 'body__detail body__detail-transform'
+            animation.className = 'animation animation__margin'
+            bodyDetail.className = 'body__detail body__detail-margin'
             initDetail()
         })
 
@@ -22,14 +22,12 @@ void function transform() {
 
     function initDetail() {
         detailWrapper.innerHTML = `
-            <div class="detail__transform">
+            <div class="detail__margin">
                 <p>
-                    一：会产生新的图层(Has an active accelerated transform animation or transition)
-                    <img src="https://tva1.sinaimg.cn/large/007S8ZIlly1gij9tuz35cj30wy0u0dhc.jpg" style="width: 400px;">
+                    一：发生Layout Shift布局偏移，因为改变了盒模型本身的结构
                 </p>
                 <p>
-                    二：动画通过修改CSS视觉格式化模型的坐标空间，发生在Composite Layers，不会触发回流、重绘
-                    <img src="https://tva1.sinaimg.cn/large/007S8ZIlly1gij9y7pslrj31b20s2my8.jpg" style="width: 300px;display: block">
+                    二：margin: 发生Layout Shift布局偏移，Recalculate Style -> Layout -> Paint -> Composite Layers
                 </p>
             </div>
         `
@@ -38,22 +36,22 @@ void function transform() {
     function initStyle() {
         const style = document.createElement('style')
         style.innerHTML = `
-            @keyframes transformAnimation {
+            @keyframes marginAnimation {
                 0% {
-                    transform: translateX(0);
+                    margin-left: 0;
                 }
 
                 50% {
-                    transform: translateX(300px);
+                    margin-left: 300px;
                 }
 
                 100% {
-                    transform: translateX(0);
+                    margin-left: 0;
                 }
             }
 
-            .animation__transform.animation {
-                animation: transformAnimation 4s infinite;
+            .animation__margin.animation {
+                animation: marginAnimation 4s infinite;
             }
         `
         document.head.appendChild(style)
